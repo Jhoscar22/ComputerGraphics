c3ef1ada34e04b235dc99b756e09acdf0fb00993
Oscar Valdés <ojvc22@gmail.com>
2022-11-22 17:59:05 -0600

    Ball is stretched and squashed in the proper direction

--- Processing/BallAnimation/sketch.js
+++ Processing/BallAnimation/sketch.js
@@ -2,9 +2,10 @@ function setup() {
     createCanvas(700, 700);
 }
 
-let v1 = { x: 100, y: 100 }
-let p1 = { x: 350, y: 600 }
-let v2 = { x: 600, y: 100 }
+const v1 = { x: 100, y: 100 }
+const p = { x: 350, y: 600 }
+const v2 = { x: 600, y: 100 }
+const stepSize = 0.05;
 
 // P = (1−t)^2 * P1 + 2*(1−t)*t*P2 + t^2 * P3
 // y = a(x - h)^2 + k
@@ -17,23 +18,23 @@ let v2 = { x: 600, y: 100 }
 
 
 
-const a1 = (p1.y - v1.y) / (p1.x - v1.x);
+const a1 = (p.y - v1.y) / (p.x - v1.x);
 const fp_x1 = (x) => {
     return 2 * a1 * x - 2 * a1 * v1.x;
 }
 let C1 = {};
-C1.x = (v1.x + p1.x) / 2;
-C1.y = v1.y + fp_x1(v1.x) * (p1.x - v1.x) / 2;
+C1.x = (v1.x + p.x) / 2;
+C1.y = v1.y + fp_x1(v1.x) * (p.x - v1.x) / 2;
 
 
 
-const a2 = (p1.y - v2.y) / (p1.x - v2.x);
+const a2 = (p.y - v2.y) / (p.x - v2.x);
 const fp_x2 = (x) => {
     return 2 * a2 * x - 2 * a2 * v2.x;
 }
 let C2 = {};
-C2.x = (v2.x + p1.x) / 2;
-C2.y = v2.y + fp_x2(v2.x) * (p1.x - v2.x) / 2;
+C2.x = (v2.x + p.x) / 2;
+C2.y = v2.y + fp_x2(v2.x) * (p.x - v2.x) / 2;
 
 function draw() {
     background(220);
@@ -41,23 +42,43 @@ function draw() {
 
     beginShape();
     vertex(v1.x, v1.y);
-    quadraticVertex(C1.x, C1.y, p1.x, p1.y);
+    quadraticVertex(C1.x, C1.y, p.x, p.y);
     endShape();
 
     beginShape();
     vertex(v2.x, v2.y);
-    quadraticVertex(C2.x, C2.y, p1.x, p1.y);
+    quadraticVertex(C2.x, C2.y, p.x, p.y);
     endShape();
 
-    for (let t = 0; t < 1.05; t += 0.05) {
-        let x = pow((1 - t), 2) * v1.x + 2 * (1 - t) * t * C1.x + pow(t, 2) * p1.x;
-        let y = pow((1 - t), 2) * v1.y + 2 * (1 - t) * t * C1.y + pow(t, 2) * p1.y;
-        circle(x, y, 5);
+    let xp = v1.x;
+    let yp = v1.y;
+    for (let t = stepSize; t < 1.01; t += stepSize) {
+        let xc = pow((1 - t), 2) * v1.x + 2 * (1 - t) * t * C1.x + pow(t, 2) * p.x;
+        let yc = pow((1 - t), 2) * v1.y + 2 * (1 - t) * t * C1.y + pow(t, 2) * p.y;
+        let _width = sqrt(pow(xc - xp, 2) + pow(yc - yp, 2));
+        let angle = atan((yc - yp) / (xc - xp));
+        push();
+        translate((xp + xc) / 2, (yp + yc) / 2);
+        rotate(angle);
+        ellipse(0, 0, _width, 12);
+        pop()
+        xp = xc;
+        yp = yc;
     }
-    for (let t = 0; t < 1.05; t += 0.05) {
-        let x = pow((1 - t), 2) * p1.x + 2 * (1 - t) * t * C2.x + pow(t, 2) * v2.x;
-        let y = pow((1 - t), 2) * p1.y + 2 * (1 - t) * t * C2.y + pow(t, 2) * v2.y;
-        circle(x, y, 5);
+    xp = p.x;
+    yp = p.y;
+    for (let t = stepSize; t < 1.01; t += stepSize) {
+        let xc = pow((1 - t), 2) * p.x + 2 * (1 - t) * t * C2.x + pow(t, 2) * v2.x;
+        let yc = pow((1 - t), 2) * p.y + 2 * (1 - t) * t * C2.y + pow(t, 2) * v2.y;
+        let _width = sqrt(pow(xc - xp, 2) + pow(yc - yp, 2));
+        let angle = atan((yc - yp) / (xc - xp));
+        push();
+        translate((xp + xc) / 2, (yp + yc) / 2);
+        rotate(angle);
+        ellipse(0, 0, _width, 12);
+        pop()
+        xp = xc;
+        yp = yc;
     }
 
 }

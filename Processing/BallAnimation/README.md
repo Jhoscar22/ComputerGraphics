ef68619f90f9919436c1ae6db07a2025bc258453
Oscar Valdés <ojvc22@gmail.com>
2022-11-23 09:45:00 -0600

    Finished Ball Animation

---
+++ Processing/BallAnimation/README.md
@@ -0,0 +1,27 @@
+# Challenge 7 - Bouncing-ball simulation
+
+## Instructions
+
+1. Clone the repository to your local machine.
+2. Open the file `index.html` in your browser.
+
+## Description
+
+We will implement an animation that will simulate a ball that is bouncing as shown in figure 4
+Your program will be showing 3 stages or scenes that will be transitioned by a mouse right-click
+Consider the concepts you saw in class and also the following that could be useful:
+gravity, refresh-rate, object deformation, timing, real-time animation, curves drawing algorithms, color, texture, ...
+Below the 3 main stages or scenes of your program
+
+- **Click 1**
+  - Define and draw your ball's trajectory and draw it. You can use 2 curves, one that is going down and one that is going up.
+
+- **Click 2**
+  - Draw a summary of your storyboard based on figures 4 and 5. Only draw some selected frames that will provide a glimpse of your animation.
+
+- **Click 3**
+   - Run your ball-bouncing simulation. Your ball will follow the pre-defined curved trajectories. Your ball will be deformed as it's going down/up.
+
+## Preview
+
+![Preview](./images/preview.gif)
--- Processing/BallAnimation/sketch.js
+++ Processing/BallAnimation/sketch.js
@@ -1,5 +1,7 @@
+//use 'esversion: 8'
 function setup() {
     createCanvas(700, 700);
+    frameRate(1);
 }
 
 const v1 = { x: 100, y: 100 }
@@ -16,8 +18,6 @@ const stepSize = 0.05;
 // f´(x) = 2*a*x - 2*a*h
 // C = ((x1+x2)/2, y1 + f´(x1)*(x2-x1)/2)
 
-
-
 const a1 = (p.y - v1.y) / (p.x - v1.x);
 const fp_x1 = (x) => {
     return 2 * a1 * x - 2 * a1 * v1.x;
@@ -25,7 +25,6 @@ const fp_x1 = (x) => {
 let C1 = {};
 C1.x = (v1.x + p.x) / 2;
 C1.y = v1.y + fp_x1(v1.x) * (p.x - v1.x) / 2;
-
 
 
 const a2 = (p.y - v2.y) / (p.x - v2.x);
@@ -36,10 +35,13 @@ let C2 = {};
 C2.x = (v2.x + p.x) / 2;
 C2.y = v2.y + fp_x2(v2.x) * (p.x - v2.x) / 2;
 
-function draw() {
-    background(220);
-    noFill()
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+let state = 0;
+
+function curves() {
     beginShape();
     vertex(v1.x, v1.y);
     quadraticVertex(C1.x, C1.y, p.x, p.y);
@@ -49,36 +51,82 @@ function draw() {
     vertex(v2.x, v2.y);
     quadraticVertex(C2.x, C2.y, p.x, p.y);
     endShape();
+}
 
-    let xp = v1.x;
-    let yp = v1.y;
-    for (let t = stepSize; t < 1.01; t += stepSize) {
-        let xc = pow((1 - t), 2) * v1.x + 2 * (1 - t) * t * C1.x + pow(t, 2) * p.x;
-        let yc = pow((1 - t), 2) * v1.y + 2 * (1 - t) * t * C1.y + pow(t, 2) * p.y;
-        let _width = sqrt(pow(xc - xp, 2) + pow(yc - yp, 2));
-        let angle = atan((yc - yp) / (xc - xp));
+async function frame(xp, yp, t, wait, mod) {
+    if (wait) {
+        await sleep(25);
+    }
+    let xc;
+    let yc;
+    let m = t % mod;
+    let _width;
+    if (t > 0.999 && t < 1.001) {
+        if (wait) {
+            background(220);
+        }
+        push();
+        translate(p.x, p.y);
+        ellipse(0, 0, 40, 12);
+        pop()
+        return {xp: p.x, yp: p.y};
+    }
+    if (0 <= t && t < 1) {
+        xc = pow((1 - t), 2) * v1.x + 2 * (1 - t) * t * C1.x + pow(t, 2) * p.x;
+        yc = pow((1 - t), 2) * v1.y + 2 * (1 - t) * t * C1.y + pow(t, 2) * p.y;
+    } else {
+        t -= 1;
+        xc = pow((1 - t), 2) * p.x + 2 * (1 - t) * t * C2.x + pow(t, 2) * v2.x;
+        yc = pow((1 - t), 2) * p.y + 2 * (1 - t) * t * C2.y + pow(t, 2) * v2.y;
+    }
+    _width = sqrt(pow(xc - xp, 2) + pow(yc - yp, 2));
+    let angle = atan((yc - yp) / (xc - xp));
+    if (0 < m && m < 0.001 || mod - 0.001 < m && m < mod) {
+        if (wait) {
+            background(220);
+        }
         push();
         translate((xp + xc) / 2, (yp + yc) / 2);
         rotate(angle);
         ellipse(0, 0, _width, 12);
         pop()
-        xp = xc;
-        yp = yc;
     }
-    xp = p.x;
-    yp = p.y;
-    for (let t = stepSize; t < 1.01; t += stepSize) {
-        let xc = pow((1 - t), 2) * p.x + 2 * (1 - t) * t * C2.x + pow(t, 2) * v2.x;
-        let yc = pow((1 - t), 2) * p.y + 2 * (1 - t) * t * C2.y + pow(t, 2) * v2.y;
-        let _width = sqrt(pow(xc - xp, 2) + pow(yc - yp, 2));
-        let angle = atan((yc - yp) / (xc - xp));
-        push();
-        translate((xp + xc) / 2, (yp + yc) / 2);
-        rotate(angle);
-        ellipse(0, 0, _width, 12);
-        pop()
-        xp = xc;
-        yp = yc;
-    }
+    return { xp: xc, yp: yc }
+}
 
+async function draw() {
+    background(220);
+    noFill()
+
+    if (state == 1) {
+        curves();
+        return;
+    } else if (state == 2) {
+        let xp = v1.x;
+        let yp = v1.y;
+        let _width;
+        for (let t = stepSize; t <= 2.0001; t += stepSize) {
+            let r = await frame(xp, yp, t, false, stepSize*4);
+            xp = r.xp
+            yp = r.yp
+        }
+    } else if (state == 3) {
+        let xp = v1.x;
+        let yp = v1.y;
+        for (let t = stepSize; t <= 2.0001; t += stepSize) {
+            let r = await frame(xp, yp, t, true, stepSize);
+            xp = r.xp
+            yp = r.yp
+        }
+    }
+}
+
+async function mousePressed() {
+    if (state == 0) {
+        state = 1;
+    } else if (state == 1) {
+        state = 2;
+    } else if (state == 2) {
+        state = 3;
+    }
 }
